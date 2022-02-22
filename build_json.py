7b415908ed2dbf4e03adc50fb6ed838974f658df
Simon Wiles <simonjwiles@gmail.com>
2022-02-21 22:08:55 -0800

    Adapt build_json.py to work with sample SUAC data

--- build_json.py
+++ build_json.py
@@ -9,8 +9,6 @@ from pathlib import Path
 import cv2
 import sewar
 
-IMG_PATH = Path("imgs")
-
 METRICS = [
     "rmse",
     "psnr",
@@ -23,34 +21,45 @@ def main():
 
     logging.basicConfig(level=logging.INFO, format="%(message)s")
 
-    images = [
+    suac_items = json.load(open("data/SUAC/SUAC_metadata.json", "r"))
+    img_base = Path("data/SUAC")
+
+    items = [
         {
-            "id": i,
-            "name": img_path.stem,
-            "path": str(img_path),
-            "img": cv2.imread(str(img_path)),
+            "id": item["item_id"],
+            "description": item["description"],
+            "path": item["image_paths"][0],
+            "img": cv2.imread(str(img_base / item["image_paths"][0])),
         }
-        for i, img_path in enumerate(IMG_PATH.glob("*.jpg"))
+        for item in suac_items
     ]
 
     links = []
-    for img1, img2 in combinations(images, 2):
+    for img1, img2 in combinations(items, 2):
         link = {
             "source": img1["id"],
             "target": img2["id"],
             "img-sim": {},
         }
+
+        if img1["img"].shape != img2["img"].shape:
+            print(img1["img"].shape, img2["img"].shape)
+            img2_cmp = cv2.resize(img2["img"], img1["img"].shape[:2][::-1])
+        else:
+            img2_cmp = img2["img"]
         for metric in METRICS:
-            logging.info(f"Comparing {img1['name']} and {img2['name']} ({metric})...")
+            logging.info(f"Comparing {img1['id']} and {img2['id']} ({metric})...")
             link["img-sim"][metric] = getattr(sewar.full_ref, metric)(
-                img1["img"], img2["img"]
+                img1["img"], img2_cmp
             )
 
-        link["name-sim"] = SequenceMatcher(None, img1["name"], img2["name"]).ratio()
+        link["name-sim"] = SequenceMatcher(
+            None, img1["description"], img2["description"]
+        ).ratio()
 
         links.append(link)
 
-    nodes = [{k: v for k, v in img.items() if k != "img"} for img in images]
+    nodes = [{k: v for k, v in item.items() if k != "img"} for item in items]
 
     min_max = {
         metric: {
